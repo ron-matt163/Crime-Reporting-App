f84c2c233cb58cd85d677010b1aef8e7329a3bf7
ron-matt163 <rohanjm01@gmail.com>
2021-06-25 19:31:13 +0530

    Added front-end of complaint form

--- controllers/citizenFeatureController.js
+++ controllers/citizenFeatureController.js
@@ -4,5 +4,10 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = app => {
     app.get('/citDashboard/:username',(req,res) => {
         res.render('citizenDashboard');
-    });    
+    });
+
+    app.get('/citizen/logComplaint',(req,res) => {
+        res.render('citizenLogComplaint');
+    });
+
 };
